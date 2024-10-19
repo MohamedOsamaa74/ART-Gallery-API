2eb38cf5a2393effbe617ce864981a977c1551a0
Mohamed Osama <Mohamed.Elsayed22@svu.edu.eg>
2024-10-19 15:43:13 +0300

    bcrypt and jwt packages installed

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -7,29 +7,35 @@ const {validateCreateProduct}= require('../validations/productValidations')
         if(error){
         return res.status(400).json({Message:error.details[0].message}); 
         }
-    const product = new Product({
-    name:req.body.name,
-    description:req.body.description,
-    price:req.body.price,
-    category:req.body.category,
-    image:req.body.image
-    });
-    const result= await product.save();
+
+        const product = new Product({
+        name:req.body.name,
+        description:req.body.description,
+        price:req.body.price,
+        category:req.body.category,
+        image:req.body.image
+        });
+
+        const result= await product.save();
         return res.status(201).json(result);
     });
 
 
     const getProductById = asyncHandler(async(req,res)=>{
         const product= await Product.findById(req.params.id);
+
         return !product?res.status(404).message("This product Not Found") :
-                        res.status(200).json(product)
-        });
+        
+        res.status(200).json(product)
+    });
 
     const getAllProducts=asyncHandler(async (req,res)=>{
             const products= await Product.find();
+
             return products.length>0?res.status(200).json(products) :
+            
             res.status(404).message("No Products Until Now")
-            });
+    });
 
 
 
