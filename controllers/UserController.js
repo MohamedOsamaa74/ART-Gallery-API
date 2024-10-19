c4d9dd5e0a27159f6b9639de7bb9ab622d7ecd10
Mohamed Osama <Mohamed.Elsayed22@svu.edu.eg>
2024-10-20 01:03:36 +0300

    Update User Data Done

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -21,7 +21,36 @@ const getAll = async (req, res) =>{
         res.status(400).json({error: err.message});
     }
 }
+
+const updateUser = async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const requestingUserId = req.user.id; // Assuming the user ID is stored in req.user.id
+
+        if (!req.body) {
+            return res.status(400).json({ message: "Data cannot be empty" });
+        }
+        if (!userId) {
+            return res.status(400).json({ message: "Invalid User Id" });
+        }
+        if (userId !== requestingUserId) {
+            return res.status(403).json({ message: "Unauthorized: You can only update your own profile" });
+        }
+
+        const userById = await user.User.findById(userId);
+        if (!userById) {
+            return res.status(400).json({ message: "User not found" });
+        }
+
+        const updatedUser = await user.User.findByIdAndUpdate(userId, req.body, { new: true }).select('-password');
+        res.status(200).json(updatedUser);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+}
+
 module.exports = {
     getById,
-    getAll
+    getAll,
+    updateUser
 }
--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -1,6 +1,10 @@
 const route = require('express').Router();
 const UserController = require('../controllers/UserController');
+const verifyToken = require('../middlewares/jwt');
 
 route.get('/getById/:id', UserController.getById);
 route.get('/getAll', UserController.getAll);
+route.put('/update/:id', verifyToken, UserController.updateUser);
+
+
 module.exports = route;
